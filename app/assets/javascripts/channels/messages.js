57e36ef3c12049c7059dc84c8067c7d9b152a71c
Michael Quint <mjq8@cornell.edu>
2018-01-04 21:35:23 -0500

    fixed styling

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,11 +1,17 @@
 App.messages = App.cable.subscriptions.create('MessagesChannel', {  
   received: function(data) {
-    // let messageSection = document.getElementById('messages');
-    // messageSection.removeClass('hidden')
     return document.getElementById('messages').append(this.renderMessage(data));
   },
 
   renderMessage: function(data) {
-    return data.user + " : " + data.message;
+    let node = document.createElement("P")
+    let bold = document.createElement("B");
+    let t1 = document.createTextNode(`${data.user} :`);
+    let t2 = document.createTextNode(` ${data.message}`);
+    bold.appendChild(t1);
+    node.appendChild(bold);
+    node.appendChild(t2);
+    
+    return node;
   }
 });
