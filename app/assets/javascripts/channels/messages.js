b2d8caa502c18521eee64c469b0bd4a3a380fe0e
Michael Quint <mjq8@cornell.edu>
2018-01-04 21:38:00 -0500

    reverted back to ES5 Syntax

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -6,8 +6,8 @@ App.messages = App.cable.subscriptions.create('MessagesChannel', {
   renderMessage: function(data) {
     var node = document.createElement("P")
     var bold = document.createElement("B");
-    var t1 = document.createTextNode(`${data.user} :`);
-    var t2 = document.createTextNode(` ${data.message}`);
+    var t1 = document.createTextNode(data.user +" : ");
+    var t2 = document.createTextNode(data.message);
     bold.appendChild(t1);
     node.appendChild(bold);
     node.appendChild(t2);
