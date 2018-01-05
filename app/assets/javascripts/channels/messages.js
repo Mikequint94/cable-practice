fde4cc56df0cb227d84f3b25232b5ce9853eb80c
Michael Quint <mjq8@cornell.edu>
2018-01-04 21:21:37 -0500

    exchange jquery for vanilla js

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,10 +1,11 @@
 App.messages = App.cable.subscriptions.create('MessagesChannel', {  
   received: function(data) {
-    $("#messages").removeClass('hidden')
-    return $('#messages').append(this.renderMessage(data));
+    // let messageSection = document.getElementById('messages');
+    // messageSection.removeClass('hidden')
+    return document.getElementById('messages').append(this.renderMessage(data));
   },
 
   renderMessage: function(data) {
-    return "<p> <b>" + data.user + ": </b>" + data.message + "</p>";
+    return data.user + " : " + data.message;
   }
 });
